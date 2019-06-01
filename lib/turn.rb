278bf12c2309bf54972ab0be83bc9784a892de50
Tracie Masek <traciemasek@gmail.com>
2019-06-01 18:23:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,7 @@ end
 def position_taken?(board, index)
   (board[index] == " ") || (board[index] == "") || (board[index] == nil) ? false: true
 end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
