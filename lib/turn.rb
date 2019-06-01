00dce91408bde144f643ee83fa11ccbb7e370665
Tracie Masek <traciemasek@gmail.com>
2019-06-01 18:18:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,11 @@ end
 def move(board, index, char="X")
   board[index] = char
 end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index) ? true : false
+end
+
+def position_taken?(board, index)
+  (board[index] == " ") || (board[index] == "") || (board[index] == nil) ? false: true
+end
